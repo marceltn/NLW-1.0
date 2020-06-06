d094e27413af2385a40fc6ddb8e4dd87d3c09710
Marcel Nakamine <marcel.nakamine@euvo.com.br>
2020-06-06 16:19:22 -0300

    Get a specific user

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,12 @@ import express from 'express'
 
 const app = express()
 
+const users = [
+  'Alice',
+  'Bob',
+  'Marcel',
+]
+
 app.get('/', () => {
   console.log('me achou!')
 })
@@ -11,11 +17,15 @@ app.get('/hello', (req, res) => {
 })
 
 app.get('/v1/users', (req, res) => {
-  return res.json([
-    'Alice',
-    'Bob',
-    'Marcel',
-  ])
+  return res.json(users)
+})
+
+app.get('/v1/users/:id', (req, res) => {
+  const user = users[Number(req.params.id)]
+
+  if(!user) return res.status(400).json({ error: true, message: 'User not found' })
+
+  return res.status(200).send(user)
 })
 
 app.post('/v1/users', (req, res) => {
