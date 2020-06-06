efc9bdf13f49a394047b2a9ebf9651ef2b176c7a
Marcel Nakamine <marcel.nakamine@euvo.com.br>
2020-06-06 20:55:47 -0300

    Enabling static folder behavior for uploads folder

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import path from 'path'
 import routes from './routes'
 
 const app = express()
 app.use(express.json())
 app.use(routes)
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`listen on port ${port}`))
