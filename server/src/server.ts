4b2360e88101465c0a8c6c460cf167808d5ac81d
Marcel Nakamine <marcel.nakamine@euvo.com.br>
2020-06-06 19:52:42 -0300

    Squashed commit of the following:
    commit b0fc6f108209e677461f75a3798e28e4eae84ebd
    Author: Marcel Nakamine <marcel.nakamine@euvo.com.br>
    Date:   Sat Jun 6 19:48:20 2020 -0300
    
        Send user by body
    
    commit 610a89ec6237283e28d795e7cec71f60be347b9d
    Author: Marcel Nakamine <marcel.nakamine@euvo.com.br>
    Date:   Sat Jun 6 18:48:47 2020 -0300
    
        Search user by name in query params
    
    commit d094e27413af2385a40fc6ddb8e4dd87d3c09710
    Author: Marcel Nakamine <marcel.nakamine@euvo.com.br>
    Date:   Sat Jun 6 16:19:22 2020 -0300
    
        Get a specific user
    
    commit bc678d9c3d6f451150aa73f3ecd297f83e41c1ee
    Author: Marcel Nakamine <marcel.nakamine@euvo.com.br>
    Date:   Sat Jun 6 16:04:33 2020 -0300
    
        Setting v1 and add Insomnia connections

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,13 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
+
+const users = [
+  'Alice',
+  'Bob',
+  'Marcel',
+]
 
 app.get('/', () => {
   console.log('me achou!')
@@ -10,12 +17,34 @@ app.get('/hello', (req, res) => {
   return res.json({ sucess: true })
 })
 
-app.get('/users', (req, res) => {
-  return res.json([
-    'Alice',
-    'Bob',
-    'Marcel',
-  ])
+app.get('/v1/users', (req, res) => {
+  const search = String(req.query.search)
+
+  if(!search) return res.status(400).json({ error: true, message: 'Query not allowed'})
+
+  const filteredUser = search ? users.filter(user => user.includes(search)) : users
+
+  return res.json(filteredUser)
+})
+
+app.get('/v1/users/:id', (req, res) => {
+  const user = users[Number(req.params.id)]
+
+  if(!user) return res.status(400).json({ error: true, message: 'User not found' })
+
+  return res.status(200).send(user)
+})
+
+app.post('/v1/users', (req, res) => {
+  const data = req.body
+
+  const user = {
+    name: data.name,
+    email: data.email,
+  }
+
+  return res.json(data)
 })
 
 app.listen(3000)
+
