276613a69cb75e7b8a2bf636a19f293880d19c38
Marcel Nakamine <marcel.nakamine@euvo.com.br>
2020-06-07 05:01:29 -0300

    Create forms to add new collect point

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,9 +1,109 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi'
+
+import './styles.css'
+import logo from '../../assets/logo.svg'
 
 class CreatePoint extends React.Component {
   render() {
     return (
-      <h1>CreatePoint</h1>
+      <div id="page-create-point">
+        <header>
+          <img src={logo} alt="Ecoleta logo"/>
+
+          <Link to="/">
+            <FiArrowLeft />
+            Voltar para home
+          </Link>
+        </header>
+
+        <form>
+          <h1>Cadastro do <br/> ponto de coleta</h1>
+
+          <fieldset>
+            <legend>
+              <h2>Dados</h2>
+            </legend>
+
+            <div className="field">
+              <label htmlFor="name">Nome da entidade</label>
+              <input type="text" name="name" id="name" />
+            </div>
+
+            <div className="field-group">
+              <div className="field">
+                <label htmlFor="email">E-mail</label>
+                <input type="email" name="email" id="email" />
+              </div>
+              <div className="field">
+                <label htmlFor="whatsapp">WhatsApp</label>
+                <input type="text" name="whatsapp" id="whatsapp"/>
+              </div>
+            </div>
+          </fieldset>
+
+          <fieldset>
+            <legend>
+              <h2>Endereço</h2>
+              <span>Selecione o endereço no mapa</span>
+            </legend>
+
+            <div className="field-group">
+              <div className="field">
+                <label htmlFor="uf">Estado (UF)</label>
+                <select name="uf" id="uf">
+                  <option value="0">Selecione uma UF</option>
+                </select>
+              </div>
+            </div>
+            <div className="field-group">
+              <div className="field">
+                <label htmlFor="city">Cidade</label>
+                <select name="city" id="city">
+                  <option value="0">Selecione uma cidade</option>
+                </select>
+              </div>
+            </div>
+          </fieldset>
+
+          <fieldset>
+            <legend>
+              <h2>Itens de coleta</h2>
+              <span>Slecione um ou mais itens de coleta</span>
+            </legend>
+
+            <ul className="items-grid">
+              <li>
+                <img src="http://localhost:3000/uploads/oleo.svg" alt="ícone óleo"/>
+                <span>Óleo de Cozinha</span>
+              </li>
+              <li>
+                <img src="http://localhost:3000/uploads/oleo.svg" alt="ícone óleo"/>
+                <span>Óleo de Cozinha</span>
+              </li>
+              <li>
+                <img src="http://localhost:3000/uploads/oleo.svg" alt="ícone óleo"/>
+                <span>Óleo de Cozinha</span>
+              </li>
+              <li>
+                <img src="http://localhost:3000/uploads/oleo.svg" alt="ícone óleo"/>
+                <span>Óleo de Cozinha</span>
+              </li>
+              <li>
+                <img src="http://localhost:3000/uploads/oleo.svg" alt="ícone óleo"/>
+                <span>Óleo de Cozinha</span>
+              </li>
+              <li>
+                <img src="http://localhost:3000/uploads/oleo.svg" alt="ícone óleo"/>
+                <span>Óleo de Cozinha</span>
+              </li>
+            </ul>
+          </fieldset>
+
+          <button type="submit">Cadastrar ponto de coleta</button>
+        </form>
+      </div>
     )
   }
 }
