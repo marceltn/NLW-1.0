cf96384a81ebb152bfcd23e2b954136361abf7c1
Marcel Nakamine <marcel.nakamine@euvo.com.br>
2020-06-06 21:27:12 -0300

    New endpoint create points to collect waste

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,19 +2,63 @@ import express from 'express'
 import knex from './database'
 const routes = express.Router()
 
-const generateResourceUrl = (resource: string): string => `/v1/${resource}`
+const generateResourceUrl = (resource: string): string => `/v1${resource}`
 
-routes.get(generateResourceUrl('items'), async (req, res) => {
+routes.get(generateResourceUrl('/items'), async (req, res) => {
   const items = await knex('items').select('*')
 
   const serializedItems = items.map(item => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:3000/uploads/${item.image}`
     }
   })
 
   return res.json(serializedItems)
+})
+
+routes.post(generateResourceUrl('/points'), async (req, res) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = req.body
+
+  const trx = await knex.transaction()
+
+  console.log('before insert points')
+
+  const [insertedId] = await trx('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  })
+
+  const point_id = insertedId
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id,
+    }
+  })
+
+  await trx('point_items').insert(pointItems)
+
+  await trx.commit()
+
+  return res.json({success: true, id: point_id})
 })
 
 export default routes
