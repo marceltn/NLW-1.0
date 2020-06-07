81b32d9465bdd6a4a07b65ab41ba941b1cdbb61c
Marcel Nakamine <marcel.nakamine@euvo.com.br>
2020-06-06 21:07:17 -0300

    Fetch all items with title and image url

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,20 @@
 import express from 'express'
+import knex from './database'
 const routes = express.Router()
 
-routes.get('/v1', (req, res) => {
-  return res.json({ sucess: true })
+const generateResourceUrl = (resource: string): string => `/v1/${resource}`
+
+routes.get(generateResourceUrl('items'), async (req, res) => {
+  const items = await knex('items').select('*')
+
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3000/uploads/${item.image}`
+    }
+  })
+
+  return res.json(serializedItems)
 })
 
 export default routes
