e555d64afd13601806c9c7344531af9a1b4ce94c
Marcel Nakamine <marcel.nakamine@euvo.com.br>
2020-06-07 04:42:44 -0300

    Adding router between Home and CreatePoint

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Home from './pages/Home'
+import Routes from './Routes'
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes />
     </div>
   );
 }
---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class CreatePoint extends React.Component {
+  render() {
+    return (
+      <h1>CreatePoint</h1>
+    )
+  }
+}
+
+export default CreatePoint
