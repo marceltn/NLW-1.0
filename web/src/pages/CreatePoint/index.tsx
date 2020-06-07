6d4e3d18f9f23c9df6294f0b598867dc0cda2907
Marcel Nakamine <marcel.nakamine@euvo.com.br>
2020-06-07 05:15:35 -0300

    Integrate leaflet map inside forms

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
+import { Map, TileLayer, Marker } from 'react-leaflet'
 
 import './styles.css'
 import logo from '../../assets/logo.svg'
@@ -48,6 +49,15 @@ class CreatePoint extends React.Component {
               <h2>Endereço</h2>
               <span>Selecione o endereço no mapa</span>
             </legend>
+
+            <Map center={{ lat: -23.5575756, lng: -46.635728}} zoom={17.5}>
+              <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+
+              <Marker position={{ lat: -23.5575756, lng: -46.635728}} />
+            </Map>
 
             <div className="field-group">
               <div className="field">
