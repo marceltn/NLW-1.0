17f2ace99f7c51078ce1b28414d8f4e96ea510f8
Marcel Nakamine <marcel.nakamine@euvo.com.br>
2020-06-07 22:10:37 -0300

    Adding Details page

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,8 +1,48 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import Constants from 'expo-constants'
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import { View, Text, Image, TouchableOpacity, SafeAreaView, StyleSheet } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
 
 const Detail = () => {
-  return <View />
+  const navigation = useNavigation()
+
+  function handleNavigateBack() {
+    navigation.goBack()
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
+
+        <Image style={styles.pointImage} source={{ uri: "https://images.unsplash.com/photo-1488459716781-31db52582fe9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"}} />
+
+        <Text style={styles.pointName}>Mercadão do João</Text>
+        <Text style={styles.pointItems}>Lâmpadas, Óleo de Cozinha</Text>
+
+        <View style={styles.address}>
+          <Text style={styles.addressTitle}>Endereço</Text>
+          <Text style={styles.addressContent}>Rio do Sul, SC</Text>
+        </View>
+
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <FontAwesome name="whatsapp" size={20} color="#FFF" />
+          <Text style={styles.buttonText}>WhatsApp</Text>
+        </RectButton>
+
+        <RectButton style={styles.button} onPress={() => {}}>
+          <Icon name="mail" size={20} color="#FFF" />
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
+  )
 }
 
 export default Detail
